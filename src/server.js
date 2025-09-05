d6a7b0eb950aaeeb5423b945508ee2ed250348ee
Dias-Gabriel-Dev <gabriel.henrique.dias@outlook.com>
2025-09-04 22:05:28 -0300

    inciando ORM com postgres e docker

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import app from './app.js';
+import { testConnection } from './config/database.js';
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Projeto GymForce API iniciado! at port ${PORT}`);
+  testConnection();
 });
+
+// Vamos continuar o projeto GymForce API. prompt para retomar o andamento

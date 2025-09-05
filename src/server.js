d93b7e90e347db5ecf7492750cee3b4a0ef71eaf
Dias-Gabriel-Dev <gabriel.henrique.dias@outlook.com>
2025-09-04 21:36:01 -0300

    feat: setup express server eith health check route using esm

--- src/server.js
+++ src/server.js
@@ -1,1 +1,7 @@
-console.log('Projeto GymForce API iniciado!');
+import app from './app.js';
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Projeto GymForce API iniciado! at port ${PORT}`);
+});
